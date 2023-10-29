38e88ad1456edadcd99ea46be3b395260eb49c81
Abirami Sukumaran <abidsukumaran@gmail.com>
2023-10-29 21:43:00 +0530

    Update getImage.js
    Added a method

--- src/main/resources/static/js/getImage.js
+++ src/main/resources/static/js/getImage.js
@@ -5,3 +5,13 @@ function goBack() {
 function callAPIOS() {
     alert("This app is open sourced. Feel free to implement this method!");
 }
+
+function loadImage() {
+    var image = new Image();
+    var baseStr64 = document.getElementById("hiddenstring").value;      
+    poseImage.setAttribute('src', "data:image/jpg;base64," + baseStr64);
+}
+
+function callAPIOS(){
+    alert("This app is open sourced. Feel free to implement this method!");
+}
