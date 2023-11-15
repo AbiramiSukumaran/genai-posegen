a373cbaae2aa01381139835e121210fe1152cfa8
Abirami Sukumaran <abidsukumaran@gmail.com>
2023-11-15 20:25:40 +0530

    Update PromptController.java

--- src/main/java/com/example/demo/PromptController.java
+++ src/main/java/com/example/demo/PromptController.java
@@ -72,8 +72,12 @@ import jakarta.xml.bind.DatatypeConverter;
 
 @RestController
 public class PromptController {
-   // private static final String get_pose_list = "https://yoga-pose-spanner-uxu5wi2jpa-uc.a.run.app/"; //Spanner
-    private static final String get_pose_list = "https://yoga-poses-test-default-rtdb.firebaseio.com/Yoga_Poses.json"; //Firebase
+
+   private static final String get_pose_list = "https://yoga-pose-spanner-uxu5wi2jpa-uc.a.run.app/"; 
+	
+/*Uncomment below line and comment out above line if you are using Firebase Database instead of Spanner*/
+  //  private static final String get_pose_list = "https://yoga-poses-test-default-rtdb.firebaseio.com/Yoga_Poses.json"; 
+    
     RestTemplate restTemplate = new RestTemplate();
     private static final Logger logger = Logger.getLogger(PromptController.class.getName());
 
@@ -180,9 +184,19 @@ public class PromptController {
                 String poseString = pose;
                 poseString = poseString.replace("{", "").replace("}", "").replace("\"", "");
                 String[] splitPoseString = poseString.split(",");
-                String name = splitPoseString[2].split(":")[1];
+		    
+		//Spanner specific snippet begins
+		String name = splitPoseString[1].split(":")[1];
+                String breath = splitPoseString[2].split(":")[1];
+                String desc = splitPoseString[3].split(":")[1];
+		//Spanner specific snippet ends
+		    
+		/* Uncomment below and comment the above 3 lines enclosed within Spanner specific snippet if you are using Firebase database instead of Spanner */
+		/*
+  		String name = splitPoseString[2].split(":")[1];
                 String breath = splitPoseString[0].split(":")[1];
                 String desc = splitPoseString[1].split(":")[1];
+		*/
                 poseList.add(name);
             }
         } catch(Exception e){
